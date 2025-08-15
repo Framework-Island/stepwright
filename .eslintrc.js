bf860a59dc42f7b3b6f5672a7920081e845fa2df
Muhammad Umer Farooq <umer@lablnet.com>
2025-08-15 19:47:22 +0500

    Update ESLint configuration and package.json scripts. Modified .eslintrc.js to disable specific TypeScript linting rules and added browser environment support. Updated package.json to ensure tests run before building during the prepublish process, enhancing the overall development workflow.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
-        'eslint:recommended',
-        '@typescript-eslint/recommended'
+        'eslint:recommended'
     ],
     plugins: ['@typescript-eslint'],
     env: {
         node: true,
-        es6: true
+        es6: true,
+        browser: true // Add browser environment for window/document
     },
     parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module'
     },
     rules: {
-        // Add any custom rules here
+        // Disable annoying rules
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        'no-empty': 'off',
+        'no-constant-condition': 'off'
     }
 };
